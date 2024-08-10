428d5e5b3bd9d11d0836437f50be4e3549985007
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2024-08-10 00:40:08 -0300

    Added optimized title state

---
+++ Assets/ScriptRevised/GameManagerV2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int rune_count = 6 * 8,
+        variations_index = 0;
+
+    private int[] variations_options;
+
+    [SerializeField]
+    private TextMeshProUGUI variations_label;
+
+    void Start()
+    {
+        variations_options = InitializeRuneCounter();
+        variations_label.text = $"{variations_options[variations_index]}";
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    private int[] InitializeRuneCounter()
+    {
+        // Load all Texture2D assets in the "Runes" folder within the Resources folder
+        Texture2D[] runeTextures = Resources.LoadAll<Texture2D>("Runes");
+
+        // Count the number of textures
+        int rune_variants = runeTextures.Length - 1;
+
+        // Use the count as a variable
+        string value = "Number of runes: " + rune_variants;
+
+        List<int> possibleVariationsList = new List<int>();
+
+        Debug.Log(value);
+
+        for (int i = 2; i < rune_variants; i++)
+        {
+            bool no_remainder = rune_count % i == 0;
+            if (no_remainder)
+            {
+                possibleVariationsList.Add(i);
+                Debug.Log($"Added: {i}");
+            }
+        }
+        return possibleVariationsList.ToArray();
+    }
+
+    public void ControlVariationsIndex(bool increase)
+    {
+        if (increase && variations_index + 1 < variations_options.Length)
+        {
+            variations_index += 1;
+        }
+        if (!increase && variations_index > 0)
+        {
+            variations_index -= 1;
+        }
+        variations_label.text = $"{variations_options[variations_index]}";
+    }
+
+        public void StartGameLoadingValues()
+    {
+        int variations=variations_options[variations_index];
+        PlayerPrefs.SetInt(CustomConstants.n_variations_index_pref,variations);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("GameState");
+    }
+}
